0766df4ba9faf45f1a79d15b194419e5c6206a0e
DiparshanBaral <np03cs4s230184@heraldcollege.edu.np>
2024-12-22 09:43:22 +0545

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="middleheader text-center font-bold text-red-400">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
